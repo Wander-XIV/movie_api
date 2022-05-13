8b67ee775547737d80e64f73912824ca5ca627ca
Wander-XIV <aroy1420@gmail.com>
2022-05-13 15:54:07 -0400

    Updates
    Added in all the working request methods. Removed the unused code.

--- index.js
+++ index.js
@@ -1,152 +1,205 @@
-const express = require("express"),
-  bodyParser = require("body-parser"),
-  uuid = require("uuid"),
-  morgan = require("morgan");
+const express = require("express");
+const bodyParser = require("body-parser");
+uuid = require("uuid");
+morgan = require("morgan");
+const mongoose = require("mongoose");
+const Models = require("./models.js");
+
+const Movies = Models.Movie;
+const Users = Models.User;
+
+mongoose.connect("mongodb://localhost:27017/test", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const app = express();
-
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-let users = [
-  {
-    id: 1,
-    username: 'J4ne',
-    email: 'Jane.Doe@mail.com',
-    password: 'vZR34!pj#a',
-    birthday: '04/12/1987',
-    favorites: [],
-  },
-  {
-    id: 2,
-    username: 'J0hn',
-    email: 'John.Smith@mail.com',
-    password: 'H98vXE!#q',
-    birthday: '06/25/1992',
-    favorites: [],
-  }
-];
-
-let movies = [
-  {
-    Title: "Dog",
-    Description:
-      "Two former Army Rangers Briggs and a Belgian Malinois named Lulu journey to a fellow soldier's funeral.",
-    Genre: {
-      Name: "Comedy",
-      Description:
-        "Comedy is a genre of fiction that consists of discourses or works intended to be humorous or amusing by inducing laughter, especially in theatre, film, stand-up comedy, television, radio, books, or any other entertainment medium.",
-    },
-
-    Director: {
-      Names: "Reid Carolin",
-      Bio: "Reid Carolin is an American film producer, director, and screenwriter.",
-      Birth: "Reid Carolin was born January 10, 1982.",
-    },
-    ImagePath:
-      "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQYfOlKIxkS4ULGULhHT7o7THm93GGoGMjAaQEjnlrzGElbX8Dp",
-    Featured: true,
-  },
-  {
-    Title: "Lord of the Rings",
-    Description:
-      "A shy young hobbit named Frodo Baggins inherits a simple gold ring thatt holds the secret to the survival--or enslavement of the entire world.",
-    Genre: {
-      Name: "Adventure",
-      Description:
-        "An adventure is an exciting experience or undertaking that is typically bold, sometimes risky.",
-    },
-
-    Director: {
-      Name: "Peter Jackson",
-      Bio: "Sir Peter Robert Jackson ONZ KNZM is a New Zealand film director, screenwriter, and film producer.",
-      Birth: "October 31, 1961",
-    },
-    ImagePath:
-      "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_.jpg",
-    Featured: true,
-  },
-
-  {
-    Title: "John Wick",
-    Description:
-      "A retired hitman is forced back into action by a sadistic younfg thug, he hunts down his adverseries with the skill and ruthlessness that made him an underworld legend.",
-    Genre: {
-      Name: "Action",
-      Description:
-        "Action film is a film genre in which the protagonist is thrust into a series of events that typically involve violence and physical feats.",
-    },
-
-    Director: {
-      Name: "Chad Stahelski",
-      Bio: "Chad Stahelski is an American stuntman and film director.",
-      Birth: "September 20, 1968",
-    },
-    ImagePath:
-      "https://m.media-amazon.com/images/M/MV5BMTU2NjA1ODgzMF5BMl5BanBnXkFtZTgwMTM2MTI4MjE@._V1_.jpg",
-    Featured: true,
-  },
-];
-
-// Gets the list of data about ALL movies
-app.get('/movies', (req,res) => {
-  res.json(movies);
+// GET: the list of data about ALL movies
+app.get("/movies", (req, res) => {
+  Movies.find()
+    .then((movies) => {
+      res.status(201).json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
 });
 
-// Gets the data about a single movie, by title
-app.get('/movies/:Title', (req,res) => {
-  res.json(movies.find((movies) =>
-  {return movies.Title === req.params.Title }));
+// GET: the data about a single movie, by title
+app.get("/movies/:Title", (req, res) => {
+  Movies.findOne({ Title: req.params.Title })
+    .then((movie) => {
+      if (movie) {
+        res.status(200).json(movie);
+      } else {
+        res.status(400).send("Movie not found.");
+      }
+    })
+    .catch((err) => {
+      res.status(500).send("Error: " + err);
+    });
 });
 
-// Gets data about a genre by name/title
-app.get('/movies/genre/:Name', (req,res) => {
-  res.send('Successful GET request that returns data on genre.');
+// GET: genre of a movie
+app.get("/genre/:Name", (req, res) => {
+  Movies.findOne({ "Genre.Name": req.params.Name })
+    .then((movie) => {
+      if (movie) {
+        res.status(200).json(movie.Genre.Description);
+      } else {
+        res.status(400).send("Genre not found.");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + error);
+    });
 });
 
-// Gets data about a director by name
-app.get('/director/:Name', (req,res) => {
-  res.send('Successful GET request that returns data on director.');
+// GET: data about a director by name
+app.get("/director/:Name", (req, res) => {
+  Movies.findOne({ "Director.Name": req.params.Name })
+    .then((movie) => {
+      if (movie) {
+        res.status(200).json(movie.Director.Bio);
+      } else {
+        res.status(400).send("Director not found.");
+      }
+    })
+    .catch((err) => {
+      res.status(500).send("Error " + err);
+    });
 });
 
-// Adds data for a new user to our list of users
+//POST: allows a user to register
 app.post('/users', (req, res) => {
-  res.send('Successful POST request that adds data for a new user.');
-  // let newUser = req.body;
-
-  // if (!newUser.name) {
-  //   const message = 'Missing name in request body';
-  //   res.status(400).send(message);
-  // } else {
-  //   newUser.id = uuid.v4();
-  //   users.push(newUser);
-  //   res.status(201).send(newUser);
-  // }
+  Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + 'already exists');
+      } else {
+        Users
+          .create({
+            Username: req.body.Username,
+            Password: req.body.Password,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+          })
+          .then((user) =>{res.status(201).json(user) })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send('Error: ' + error);
+        })
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
 });
 
-// Update the "username" of a user
-app.put('/users/:Username', (req, res) => {
-  res.send('Successful PUT request that updates a users username.')
+// GETS: all users
+app.get("/users", (req, res) => {
+  Users.find()
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
 });
 
-// Add movies to users list of favorites
-app.post('/users/:Username/movies/:MovieID', (req,res) => {
-  res.send('Successful POST request to add a movie to users favorites.');
+// GETS: a user by username
+app.get("/users/:Username", (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
 });
 
-// Removes movie from users list of favorites
-app.delete('/users/:Username/movies/:MovieID', (req,res) => {
-  res.send('Successful DELETE request to remove movie from users favorites.');
+// PUT: update a user's info, by username
+app.put("/users/:Username", (req, res) => {
+  Users.findOneAndUpdate(
+    { Username: req.params.Username },
+    {
+      $set: {
+        Username: req.body.Username,
+        Password: req.body.Password,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday,
+      },
+    },
+    { new: true }
+  ) //returns the updated document
+    .then((updatedUser) => {
+      res.status(200).json(updatedUser);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error " + err);
+    });
 });
 
-// Deletes a user from our list by ID
-app.delete('/users/:id', (req, res) => {
-  res.send('Successful DELETE request that deletes a user from list by ID.');
-  // let user = users.find((user) => { return user.id === req.params.id });
+// POST: a movie to a user's list of favorites
+app.post("/users/:Username/movies/:MovieID", (req, res) => {
+  Users.findOneAndUpdate(
+    { Username: req.params.Username },
+    {
+      $push: { FavoriteMovies: req.params.MovieID },
+    },
+    { new: true }, // This line makes sure that the updated document is returned
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      } else {
+        res.json(updatedUser);
+      }
+    }
+  );
+});
 
-  // if (user) {
-  //   users = users.filter((obj) => { return obj.id !== req.params.id });
-  //   res.status(201).send('User ' + req.params.id + ' was deleted.');
-  //}
+// DELETE: movie from users list of favorites
+app.delete("/users/:Username/movies/:MovieID", (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username },
+    {
+      $pull: { FavoriteMovies: req.params.MovieID },
+    },
+    { new: true }, // This line makes sure that the updated document is returned
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      } else {
+        res.json(updatedUser);
+      }
+    }
+  );
+});
+
+// DELETE: a user by username
+app.delete('/users/:Username', (req, res) => {
+  Users.findOneAndRemove({ Username: req.params.Username })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.Username + ' was not found');
+      } else {
+        res.status(200).send(req.params.Username + ' was deleted.');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
 // Logger
@@ -159,10 +212,6 @@ app.get("/", (req, res) => {
 
 app.get("/documentation", (req, res) => {
   res.sendFile("public/documentation.html", { root: __dirname });
-});
-
-app.get("/movies", (req, res) => {
-  res.json(movies);
 });
 
 app.use(
