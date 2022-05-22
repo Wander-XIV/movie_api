8f6e101e0ff681da9fc1b7924aa5df128b413022
Wander-XIV <aroy1420@gmail.com>
2022-05-22 06:00:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -271,7 +271,6 @@ app.use((err, req, res, next) => {
 });
 
 //Listen for request
-const port = process.env.PORT || 8080;
-app.listen(port, '0.0.0.0',() => {
- console.log('Listening on Port ' + port);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
