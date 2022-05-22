739e849b99c9604cdceb4ef115e6a3252b4db15f
Wander-XIV <aroy1420@gmail.com>
2022-05-22 16:52:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ require('./passport');
 
 
 // GET: the list of data about ALL movies
-app.get("/movies", passport.authenticate('jwt', {session: false }), (req, res) => {
+app.get('/movies', (req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
